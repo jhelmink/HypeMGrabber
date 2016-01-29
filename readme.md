267619d9a1814e36dc8704e3695f8ca1150faead
Josh <josh@agilemediaware.com>
2016-01-29 14:43:22 +1300

    First working commit
    Successfully saves single track hypem url

---
+++ readme.md
@@ -0,0 +1,26 @@
+Code
+-------
+The interesting code all resides in `server.js`
+Change the `url` variable to the track you want to download (this must be a single track hypem page url for now)
+
+
+Running
+-------
+
+0. Run `LaunchNodeServer.bat` to launch the node webserver
+0. Visit [http://localhost:8081/scrape](http://localhost:8081/scrape) in a browser to trigger the scrape
+
+
+Known Issues
+-------
+0. When getting the track stream json data we are getting blocked by hypem
+
+
+Future Development Plans
+-------
+
+0. Support for multiple track urls (eg hypem favourites or popular) - download all tracks
+0. UI for pasting the url to process
+0. UI for selecting which tracks found at the url to download
+0. Setting for number of concurrent downloads
+0. Setting for number of Favourites pages to scrape
---
+++ server.js
@@ -0,0 +1,128 @@
+// 3rd Party dependencies
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
+// Core dependencies
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var app = express();
+
+// Globals
+var artist, title, remix, source_url, stream_url;
+var downloadDir = './downloads/';
+
+// Let's scrape a single track for testing
+var url = 'http://hypem.com/track/2f0fh';
+
+// Scrape the page url for hypem tracks
+// https://scotch.io/tutorials/scraping-the-web-with-node-js
+app.get('/scrape', function (req, res) {
+	console.log('Starting scrape');
+
+	// We need to use control flow for async calls to the hypem server
+	// https://github.com/caolan/async#seriestasks-callback
+	// http://www.sebastianseilund.com/nodejs-async-in-practice
+	async.series([
+	    function(callback) { // First we get the Track details and stream source URL
+			getMainPageData(url, callback);
+		},
+		function(callback) { // Now we get the track stream data from the source url
+			getStreamData(source_url, callback);
+		},
+		function(callback) { // Finally download the file using wget for the stream
+			downloadFileWget (stream_url, callback);
+		},
+	]);
+});
+
+function getMainPageData(url, callback) {
+	console.log('Get Main Page Data - url: ' + url);
+	request(url, function (error, response, html) {
+		if (!error) {
+			var $ = cheerio.load(html);
+
+			$('.artist').filter(function () {
+				var data = $(this);
+				artist = data.text();
+			})
+
+			$('.base-title').filter(function () {
+				var data = $(this);
+				title = data.text();
+			})
+
+			remix = ''; // If there is no remix
+			$('.remix-link').filter(function () {
+				var data = $(this);
+				remix = data.text();
+			})
+
+			$('#displayList-data').filter(function () {
+				var data = $(this);
+				var scriptData = JSON.parse(data.text());
+				var id = scriptData.tracks[0].id;
+				var key = scriptData.tracks[0].key;
+				source_url = 'http://hypem.com/serve/source/' + id + '/' + key;
+				console.log('Source URL: ' + source_url);
+			})
+
+			console.log('Track to process: ' + getFileName());
+			
+		} else {
+			console.log('Failed to request url: ' + url);
+		}
+		
+		callback();
+	})
+}
+
+function getStreamData(source_url, callback) {
+	console.log('Get Stream Data - url:' + source_url);
+	request(source_url, function (error, response, jsonData) {	
+		if (!error) {
+			if(jsonData.indexOf('Error 403') > -1) {
+				console.log('We be blocked.');
+			} else {
+				var trackStreamData = JSON.parse(jsonData);
+				stream_url = trackStreamData.url;
+				console.log('Stream URL: ' + stream_url);
+			}
+		} else {
+			console.log('Failed to request track json data from source url: ' + url);
+		}
+		
+		callback();
+	})
+}
+
+// Use wget to do the download as it handles streams
+// http://www.hacksparrow.com/using-node-js-to-download-files.html
+function downloadFileWget (stream_url, callback) {
+	var fileName = getFileName();
+	// Compose the wget command
+	var wget = 'wget --output-document="' + downloadDir + fileName + '.mp3" ' + stream_url;
+	console.log(wget);
+	// Execute wget using child_process exec function
+	var child = exec(wget, function (err, stdout, stderr) {
+			if (err)
+				throw err;
+			else
+				console.log(fileName + ' downloaded to ' + downloadDir);
+		});
+	
+	callback();
+}
+
+function getFileName() {
+	var fileName = artist + ' - ' + title;
+	if(remix != '') {
+		fileName = fileName + ' (' + remix + ')';
+	}
+	return fileName;
+}
+
+app.listen('8081');
+console.log('Visit http://localhost:8081/scrape in a browser to start');
+exports = module.exports = app;
